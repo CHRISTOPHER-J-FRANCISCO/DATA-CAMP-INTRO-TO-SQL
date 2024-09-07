4738308db721eea9ef19995cf28ba690f96e4aae
Christopher <cjfg00gl3@gmail.com>
2024-09-06 21:19:31 -0400

    DELETE datacamp folder

--- RELATIONAL-DATABASES/CREATE-HOSPITAL-EMERGENCY-ROOM-DB.sql
+++
@@ -1,9 +0,0 @@
-IF EXISTS (SELECT * FROM sys.databases WHERE name = 'HospitalEmergencyRoomDB')
-    BEGIN
-        PRINT 'DATABASE HospitalEmergencyRoomDB HAS BEEN CREATED!';
-    END
-ELSE
-    BEGIN
-        CREATE DATABASE HospitalEmergencyRoomDB;
-        PRINT 'DATABASE HospitalEmergencyRoomDB CREATED!';
-    END
--- RELATIONAL-DATABASES/test.sql
+++
@@ -1,5 +0,0 @@
-USE HospitalEmergencyRoomDB;
-GO
-
-INSERT INTO dbo.Patients (PatientID, Name, Age, Sex)
-VALUES (1, 'Christopher Francisco', 17, 'M')
